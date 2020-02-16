3256c71925a498251a04ee69c340786993901e72
adamBernat <59315587+adamBernat@users.noreply.github.com>
2020-02-16 15:54:44 +0100

    changing cube root program to function (import math required)

--- cubeRoot.py
+++ cubeRoot.py
@@ -1,53 +1,68 @@
+#required
 import math
 
-x = input('input number to be cube root found: ')
 
-#creating divisor for cube root base seek
-divisor = "0."
-for i in range (len(x)-2):
-	divisor = divisor + "0"
-divisor = divisor + "1"
-print(str(divisor))
+def cubeRoot():
+	x = input('input number to be cube root found: ')
 
-
-# OLD creating addition step for calculation of root OLD!!!!
-#divisor2 = "0.00"
-#for i in range (len(x)-2):
-#	divisor2 = divisor2 + "0"
-#divisor2 = divisor2 + "1"
+	#creating divisor for cube root base seek
+	divisor = "0."
+	for i in range (len(x)-2):
+		divisor = divisor + "0"
+	divisor = divisor + "1"
+	#print(str(divisor))
 
 
-#creating addition step for calculation of root
-if len(x) <= 3:
-	divisor2 = "0.0000001"
+	# OLD creating addition step for calculation of root OLD!!!!
+	#divisor2 = "0.00"
+	#for i in range (len(x)-2):
+	#	divisor2 = divisor2 + "0"
+	#divisor2 = divisor2 + "1"
+
+
+	#creating addition step for calculation of root
+	if len(x) <= 3:
+		divisor2 = "0.0000001"
 	
-if (len(x) <= 5) and (len(x) > 3):
-	divisor2 = "0.000001"
+	if (len(x) <= 5) and (len(x) > 3):
+		divisor2 = "0.000001"
 
-if (len(x) <= 6) and (len(x) > 5):
-	divisor2 = "0.00001"
+	if (len(x) <= 6) and (len(x) > 5):
+		divisor2 = "0.00001"
 
-if (len(x) <= 7) and (len(x) > 6):
-	divisor2 = "0.0001"
+	if (len(x) <= 8) and (len(x) > 6):
+		divisor2 = "0.0001"
 
-if len(x) > 7:
-	divisor2 = "0.0001"
+	if (len(x) <= 10) and (len(x) > 8):
+		divisor2 = "0.0001"
 
-#creating smallest number to be compared to cube root candidate
-seeked = float(x) * float(divisor)
+	if len(x) > 10:
+		divisor2 = "0.0001"
 
-#Declaring variable which holds calculated cube root until it reach (by power of three) input number
-seeked3 = 1 
-x = int(x)
+	#creating smallest number to be compared to cube root candidate
+	seeked = float(x) * float(divisor)
 
-
-while  seeked3 < x:
-	seeked = seeked + float(divisor2)
-	seeked3= seeked * seeked
-	seeked3= seeked3 * seeked
+	#Declaring variable which holds calculated cube root until it reach (by power of three) input number
+	seeked3 = 1 
 
 
+	#if len(x) <= 10:
+	while  (seeked3 + float(divisor2)) < int(x):
+		seeked = seeked + float(divisor2)
+		seeked3= seeked ** 3
 
-print("calculated cube root is: " + str(seeked))
-print("cube root to ^3 is: " + str(seeked3))
-print("math pow result is: " + str(math.pow(x,1/3)))
+	#if len(x) > 10:
+	#	while  (seeked3 + float(divisor2)) < int(x):
+	#		seeked = seeked + float(divisor2)
+	#		seeked3= seeked ** 3
+
+
+	# result printing
+	print("calculated cube root is: " + str(seeked))
+	print("cube root to ^3 is: " + str(seeked3) + "\n")
+
+	# comparision to math.pow method
+	mathpowresult = math.pow(int(x),1/3)
+	mathpowresultCubed = mathpowresult ** 3
+	print("calculated math pow result is: " + str(mathpowresult))
+	print("math.pow cube root to ^3 is: " + str(mathpowresultCubed))
