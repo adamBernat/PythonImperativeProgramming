f053aff3bbfeaee09b00b1334dfe8f3ca7ead91d
adamBernat <59315587+adamBernat@users.noreply.github.com>
2020-02-16 15:55:31 +0100

    adding Cube Root functionality to main project (Menu '7')

--- project.py
+++ project.py
@@ -1,4 +1,5 @@
 import random
+import math
  
  
 def help1():
@@ -9,6 +10,7 @@ def help1():
     print(" 4. Collatz Conjecture Simulation (type '4')")
     print(" 5. Gapful number checker (type '5')")
     print(" 6. Summations calculator (type '6')")
+    print(" 7. Cube root calculator (type '7')")
     print(" y. To end program type 'q'")
     print(" z. To acces help type 'h'")
  
@@ -280,6 +282,71 @@ def summations():
     summCalc(*input("please input string like '2 6 *3' \nWhere first digit is lower bound\nSecond digit is upper bound\nThird string is calculation type.\nRemember to split up variables with one space\nInput: ").split(" "))
     print("Subprogram finished returning to program menu")
 
+def cubeRoot():
+	x = input('input number to be cube root found: ')
+
+	#creating divisor for cube root base seek
+	divisor = "0."
+	for i in range (len(x)-2):
+		divisor = divisor + "0"
+	divisor = divisor + "1"
+	#print(str(divisor))
+
+
+	# OLD creating addition step for calculation of root OLD!!!!
+	#divisor2 = "0.00"
+	#for i in range (len(x)-2):
+	#	divisor2 = divisor2 + "0"
+	#divisor2 = divisor2 + "1"
+
+
+	#creating addition step for calculation of root
+	if len(x) <= 3:
+		divisor2 = "0.0000001"
+	
+	if (len(x) <= 5) and (len(x) > 3):
+		divisor2 = "0.000001"
+
+	if (len(x) <= 6) and (len(x) > 5):
+		divisor2 = "0.00001"
+
+	if (len(x) <= 8) and (len(x) > 6):
+		divisor2 = "0.0001"
+
+	if (len(x) <= 10) and (len(x) > 8):
+		divisor2 = "0.0001"
+
+	if len(x) > 10:
+		divisor2 = "0.0001"
+
+	#creating smallest number to be compared to cube root candidate
+	seeked = float(x) * float(divisor)
+
+	#Declaring variable which holds calculated cube root until it reach (by power of three) input number
+	seeked3 = 1 
+
+
+	#if len(x) <= 10:
+	while  (seeked3 + float(divisor2)) < int(x):
+		seeked = seeked + float(divisor2)
+		seeked3= seeked ** 3
+
+	#if len(x) > 10:
+	#	while  (seeked3 + float(divisor2)) < int(x):
+	#		seeked = seeked + float(divisor2)
+	#		seeked3= seeked ** 3
+
+
+	# result printing
+	print("calculated cube root is: " + str(seeked))
+	print("cube root to ^3 is: " + str(seeked3) + "\n")
+
+	# comparision to math.pow method
+	mathpowresult = math.pow(int(x),1/3)
+	mathpowresultCubed = mathpowresult ** 3
+	print("calculated math pow result is: " + str(mathpowresult))
+	print("math.pow cube root to ^3 is: " + str(mathpowresultCubed))
+
 
 
 #program
@@ -310,6 +377,10 @@ while Quit != True:
 
     if MainMenu=="6":
         summations()
+        MainMenu=""
+
+    if MainMenu=="7":
+        cubeRoot()
         MainMenu=""
        
     if MainMenu=="q":
